2408a077ad71445a5d88f3231c0d18419cbb0753
superucitelka <dokonala.hilda@gmail.com>
2021-03-17 23:33:09 +0100

    05 - views, templates - uvodni stranka

--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-
+from movies.models import Film, Genre, Attachment
 
 def index(request):
-    """
-    View function for home page of site.
-    """
-    # Render the HTML template index.html
-    return render(
-        request,
-        'index.html',
-    )
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_films = Film.objects.all().count()
+    films = Film.objects.order_by('-rate')[:3]
+
+    context = {
+        'num_films': num_films,
+        'films': films
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
 
 def topten(request):
     """
