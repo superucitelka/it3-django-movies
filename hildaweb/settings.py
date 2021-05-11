f1852b8f805f1db74a40bb32c1550c64f005fa6f
superucitelka <dokonala.hilda@gmail.com>
2021-05-11 08:58:49 +0200

    10 - autentizace

--- hildaweb/settings.py
+++ hildaweb/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'hildaweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -140,3 +140,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Umístění uploadovaných souborů
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/'
--- hildaweb/urls.py
+++ hildaweb/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('', RedirectView.as_view(url='movies/')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
